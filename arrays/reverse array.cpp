1bd58073bc72d552d577540e49d9d7af014f99bd
Architha Ramesh <01.architha@gmail.com>
2026-04-09 20:45:40 +0530

    Update and rename 1.cpp to reverse array.cpp

---
+++ arrays/reverse array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i;
+    cout<<"enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    for (i=0;i<n;i++){
+        cout<<"enter the input: ";
+        cin>>arr[i];
+    }
+    for(i=n-1; i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
